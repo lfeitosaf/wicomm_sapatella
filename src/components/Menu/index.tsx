ad6b765ab8730120fc990ce1ea33e8a25b354ecd
Lucas Miranda Feitosa <50138385+lfeitosaf@users.noreply.github.com>
2024-04-21 18:28:28 -0300

    fix: textbutton

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,59 +1,30 @@
 import { useTheme } from "styled-components";
 import * as S from "./styles";
-import Text from "../Text";
+import TextButton from "../TextButton";
+
+interface Menu {
+  id: number;
+  title: string;
+}
+
+const MenuArray: Menu[] = [
+  { id: 1, title: "Novidades" },
+  { id: 2, title: "Sapatos" },
+  { id: 2, title: "Sandálias" },
+  { id: 3, title: "Tênis" },
+  { id: 4, title: "Bolsas & Acessórios" },
+  { id: 5, title: "OFF" },
+];
 
 const Menu = () => {
   const theme = useTheme();
   return (
     <S.MenuList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          Novidades
-        </Text>
-      </S.ItemList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          Sapatos
-        </Text>
-      </S.ItemList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          Sandálias
-        </Text>
-      </S.ItemList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          Tênis
-        </Text>
-      </S.ItemList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          Bolsas & Acessórios
-        </Text>
-      </S.ItemList>
-      <S.ItemList>
-        <Text
-          size={theme.typography.sizes.normal}
-          color={theme.colors.brand.white}
-        >
-          OFF
-        </Text>
-      </S.ItemList>
+      {MenuArray.map((item) => (
+        <S.ItemList key={item.id}>
+          <TextButton color={theme.colors.brand.white}>{item.title}</TextButton>
+        </S.ItemList>
+      ))}
     </S.MenuList>
   );
 };
